e8103a035e1472ddb767cb7863515099aadb9d5d
nicovon24 <nicovon24@gmail.com>
2023-03-10 15:24:34 -0300

    nico

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("2ijhasdhisaoasdp")
+
+console.log("2ijhasdhisaoasdp")
+
+console.log("2ijhasdhisaoasdp")
+
+console.log("2ijhasdhisaoasdp")
+
+console.log(1234567465);
+
+console.log("nico");
